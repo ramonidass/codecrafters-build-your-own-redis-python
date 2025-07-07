9382435cb00b9950d69f41a20fe08438e9c6cacc
ramon-almeida <ramon.almbtc@proton.me>
2025-07-07 20:32:33 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -6,7 +6,8 @@ def main():
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     connection, _ = server_socket.accept()
-    connection.sendall(b"+PONG\r\n")
+    for command in connection:
+        connection.sendall(b"+PONG\r\n")
 
 
 if __name__ == "__main__":
